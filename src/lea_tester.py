54f05eef861ebd7783d52ad2e0368fef78cfee6e
lj8128 <leejs8128@gmail.com>
2023-04-25 19:42:47 +0000

    debugged LEA; designed and implemented re-election procedures

--- src/lea_tester.py
+++ src/lea_tester.py
@@ -14,7 +14,6 @@ def hegemony_handler(msg):
 if __name__== '__main__':
     rospy.init_node("lea_tester")
     mps = {'roba', 'robb', 'robc', 'rafael'}
-    time.sleep(5)
     for mp in mps:
         rospy.Subscriber(f'{mp}_hegemony_broadcasts', String, hegemony_handler)
     rospy.spin()
--- src/mp.py
+++ src/mp.py
@@ -19,6 +19,11 @@ class MP:
         self.majority = self.mp_count // 2 + 1
         self.other_mps = {'roba', 'robb', 'robc', 'rafael'}
         self.other_mps.remove(self.name)
+        
+        #=====For resignation/re-election testing====================
+        self.resign = False
+        rospy.Subscriber('keys', String, self.__key_input_handler)
+        #============================================================
 
         self.hegemony_publisher = rospy.Publisher(f'{self.name}_hegemony_broadcasts', String, queue_size=10)
 
@@ -26,7 +31,6 @@ class MP:
         self.homage_request_publisher = rospy.Publisher(f'{self.name}_homage_requests', String, queue_size=10)
 
         self.vote_reply_publisher_to = {}
-        self.homage_reply_publisher_to = {}
         for mp_name in self.other_mps:
             rospy.Subscriber(f'{mp_name}_vote_requests', String, self.__vote_request_handler)
             self.vote_reply_publisher_to[mp_name] = rospy.Publisher(f'{self.name}_vote_replies_to_{mp_name}', String, queue_size=10)
@@ -39,7 +43,6 @@ class MP:
             if self.state == 'follower':
                 self.__follow()
             elif self.state == 'candidate':
-                print(f'candidate: {self.name}')
                 self.__start_election()
             elif self.state == 'leader':
                 self.__lead()
@@ -66,20 +69,20 @@ class MP:
 
         self.__wait_for_replies(ballot)
 
-        if ballot.yea >= self.majority:
+        if ballot.yea >= self.majority and self.state != 'follower':
             self.state = 'leader'
         else:
-            self.state = 'follower'
-            self.paid_homage_at = rospy.Time.now()
-            self.election_timeout = self.__get_rand_duration(2000, 3000)
+            self.__become_follower()
 
         for sub in subs:
             sub.unregister()
 
     def __lead(self):
-        self.homage_request_publisher.publish(f'{self.term}, {self.name}')
-        self.hegemony_publisher.publish(self.name)
-
+        if not self.resign:
+            self.homage_request_publisher.publish(f'{self.term}, {self.name}')
+            self.hegemony_publisher.publish(self.name)
+        else:
+            self.__become_follower()
 
     #=======Callback Methods=======
 
@@ -87,6 +90,7 @@ class MP:
         received_term, candidate = self.__parse_message(msg)
         self.__update_term(received_term)
         if received_term >= self.term and (self.voted_for=='' or self.voted_for==candidate):
+            self.voted_for = candidate
             self.vote_reply_publisher_to[candidate].publish(f'{self.term}, yea')
         else:
             self.vote_reply_publisher_to[candidate].publish(f'{self.term}, nay')
@@ -101,6 +105,10 @@ class MP:
             self.paid_homage_at = rospy.Time.now()
             self.state = 'follower'
             self.voted_for = ''
+
+    def __key_input_handler(self, msg):
+        if msg.data[0] == 'r':
+            self.resign = True
 
     #=======Helper Methods=======
 
@@ -142,6 +150,13 @@ class MP:
                 ballot.nay += 1
         finally:
             ballot.mutex.release()
+
+    def __become_follower(self):
+        self.state = 'follower'
+        self.voted_for = ''
+        self.paid_homage_at = rospy.Time.now()
+        self.election_timeout = self.__get_rand_duration(2000, 3000)
+        self.resign = False
 
 class Ballot:
     def __init__(self):
